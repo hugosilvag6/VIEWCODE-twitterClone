51203347583bbba57aa4b7fe409981d3fd70cdb4
hugosilvag6 <hugosilvag6@hotmail.com>
2022-11-28 16:57:21 -0300

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Hugo Silva on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
